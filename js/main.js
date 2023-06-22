8521213dbc2f60c879dc1a3548b29e4a14ac160c
rajaguru27 <jprsgurujprsguru@gmail.com>
2023-06-22 13:21:52 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var btn =document.getElementById("btn");
+var del =document.getElementById("del");
+var button =document.getElementById("button");
+var send =document.getElementById("send");
+var click =document.getElementById("Click");
+
+btn.addEventListener("click",function() {
+   click.style.display="block";
+
+})
+button.addEventListener("click",function(){
+   click.style.display="none";
+})
+del.addEventListener("click",function(){
+  click.style.display="none";
+})
